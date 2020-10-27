b1e2b2aede9b3a6ee8cd5c770723031385356a58
dogaberkayerdek <57464323+dogaberkayerdek@users.noreply.github.com>
2020-10-28 02:38:26 +0300

    Add files via upload

---
+++ alistirma13.py
@@ -0,0 +1,14 @@
+for x in range(10,100):
+    for y in range(10,100):
+        x=str(x)
+        y=str(y)
+
+        toplam=x+y
+        if (int(toplam)) == (11*(int(x) + int(y))):
+            print(int(x),int(y))
+            
+   
+    
+    
+    
+
