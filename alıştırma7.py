a9bfa61930b4d6739271489a468991c0f030d113
dogaberkayerdek <57464323+dogaberkayerdek@users.noreply.github.com>
2020-10-28 01:09:21 +0300

    Add files via upload

---
+++ alıştırma7.py
@@ -0,0 +1,9 @@
+k=0
+for x in range(100,1000):
+    x=str(x)
+    if int(x[0]) + int(x[1]) == int(x[2]):
+        print(int(x))
+        k+=1
+print("Toplamda", k , "sayı vardır")
+    
+                                   
