d100969720db34c3dc929ef03ff7f5e2324ebee1
dogaberkayerdek <57464323+dogaberkayerdek@users.noreply.github.com>
2020-11-21 04:34:42 +0300

    Add files via upload

--- alistirma14.py
+++ alistirma14.py
@@ -1,12 +1,23 @@
 #çarpımları 12121 olacak. a*b=12121 ve mutlak değer a-b minimum olacak ,o zaman mesela 3 basamak ve 2 basamaklı sayılara bakalım
-sayilar=[]
+xler=[]
+yler=[]
+farklar=[]
 for x in range(10,1000):
     for y in range(10,1000):
-        if x*y == 12121:
-            sayilar.append(abs(x-y))
+        if x*y == 121212:
+            xler.append(x)
+            yler.append(y)
+            farklar.append(abs(x-y))
+            m=farklar.index(min(farklar))
+print(xler[m],yler[m])
 
-print(min(sayilar))
-                    
+            
+
+
+            
+            
+            
+           
             
 
 
